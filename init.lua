217812be4d7846137fa1f8790cd7cda036818e29
Marco Link <mfl-github@bitblox.de>
2018-01-02 15:46:59 +0100

    Extracted settings to separate file.
    Defaults in settings.defaults.lua will be read first and can be
    overwritten by creating a file settings.lua.
    
    settings.lua added to gitignore to avoid commiting it to the repo by
    accident.

--- init.lua
+++ init.lua
@@ -1,21 +1,29 @@
--- basic_robot by rnd, 2016
+local function file_exists(name)
+  local file = io.open(name,"r")
+
+  if file ~= nil then
+    io.close(file)
+    return true
+  end
+
+  return false
+end
 
 local modName = minetest.get_current_modname()
 
 basic_robot = {};
-----  SETTINGS  ------
-basic_robot.call_limit = 48; -- how many execution calls per script run allowed
-basic_robot.entry_count = 2 -- how many robots ordinary player can have
-basic_robot.advanced_count = 16 -- how many robots player with robot privs can have
-basic_robot.radius = 32; -- divide whole world into blocks of this size - used for managing events like keyboard punches
-basic_robot.password = "password"; -- IMPORTANT: change it before running mod, password used for authentifications
 
-basic_robot.bad_inventory_blocks = { -- disallow taking from these nodes inventories to prevent player abuses
-	["craft_guide:sign_wall"] = true,
-}
-basic_robot.maxoperations = 3; -- how many operations (dig, generate energy,..) available per run,  0 = unlimited
-basic_robot.dig_require_energy = false; -- does robot require energy to dig?
-----------------------
+local defaultSettingsFilename = minetest.get_modpath(modName).."/settings.default.lua";
+dofile(defaultSettingsFilename)
+
+local customSettingsFilename = minetest.get_modpath(modName).."/settings.lua"
+
+if file_exists(customSettingsFilename) then
+  minetest.log("info", "Loading custom settings ...")
+  dofile(customSettingsFilename)
+else
+  minetest.log("warning", "WARNING! No custom settings found in "..customSettingsFilename..". Using defaults from "..defaultSettingsFilename.."!")
+end
 
 basic_robot.http_api = minetest.request_http_api(); 
 
---
+++ settings.default.lua
@@ -0,0 +1,11 @@
+basic_robot.call_limit = 48; -- how many execution calls per script run allowed
+basic_robot.entry_count = 2 -- how many robots ordinary player can have
+basic_robot.advanced_count = 16 -- how many robots player with robot privs can have
+basic_robot.radius = 32; -- divide whole world into blocks of this size - used for managing events like keyboard punches
+basic_robot.password = "password"; -- IMPORTANT: change it before running mod, password used for authentifications
+
+basic_robot.bad_inventory_blocks = { -- disallow taking from these nodes inventories to prevent player abuses
+  ["craft_guide:sign_wall"] = true,
+}
+basic_robot.maxoperations = 3; -- how many operations (dig, generate energy,..) available per run,  0 = unlimited
+basic_robot.dig_require_energy = false; -- does robot require energy to dig?
